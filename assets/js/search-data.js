5db261b139f985849a5f040fce2bd7599f985a57
Wyyyb <yubo.wang.sunny@gmail.com>
2025-07-19 05:29:17 +0000

    Deploying to gh-pages from @ Wyyyb/wyyyb.github.io@8c9dc7b93fc16bc77ff4efc7e84f76956163665f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-mmlu-pro-https-arxiv-org-abs-2406-01574-is-accepted-to-neurips-2024-https-proceedings-neurips-cc-paper-files-paper-2024-hash-ad236edc564f3e3156e1b2feafb99a24-abstract-datasets-and-benchmarks-track-html",
+            },},{id: "news-mmlu-pro-https-arxiv-org-abs-2406-01574-is-accepted-to-neurips-2024-https-proceedings-neurips-cc-paper-files-paper-2024-hash-ad236edc564f3e3156e1b2feafb99a24-abstract-datasets-and-benchmarks-track-html",
           title: '🎉 [MMLU-Pro](https://arxiv.org/abs/2406.01574) is accepted to [Neurips 2024](https://proceedings.neurips.cc/paper_files/paper/2024/hash/ad236edc564f3e3156e1b2feafb99a24-Abstract-Datasets_and_Benchmarks_Track.html)',
+          description: "",
+          section: "News",},{id: "news-i-arrived-at-the-university-of-waterloo-and-started-my-ph-d-journey",
+          title: 'I arrived at the University of Waterloo and started my Ph.D. journey!',
           description: "",
           section: "News",},{id: "news-critique-fine-tuning-and-scholarcopilot-are-accepted-to-colm-2025",
           title: '🎉 Critique Fine-Tuning and ScholarCopilot are accepted to COLM 2025',
